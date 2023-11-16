c859626d9201d158f1b5c61fa9463cb91b2631ad
JohanAndres19 <johanandres@hotmail.com>
2023-11-16 11:40:47 -0500

    configuracion cors

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,9 +1,11 @@
+import 'package:angel3_cors/angel3_cors.dart';
 import 'package:angel3_framework/angel3_framework.dart';
 import 'package:file/file.dart';
 import 'controllers/controlers.dart' as controllers;
 
 AngelConfigurer configureServer(FileSystem fileSystem) {
   return (Angel app) async {
+    app.fallback(cors());
     await app.configure(controllers.configureServer);
     app.get('/', (req, res) => res.write("hola mundo"));
     app.fallback((req, res) => throw AngelHttpException.notFound());
--- lib/src/services/DB/scriptsDB/user.sql
+++ lib/src/services/DB/scriptsDB/user.sql
@@ -1,8 +1,1 @@
--- Crear inserción de 500 usuarios
-INSERT INTO Usuario (campo1, campo2, campo3, campo4, campo5)
-VALUES
-    ('20181020073', 'jane', 'Doe', 'jane.doe@example.com', 'janedoe12345'),
-    -- Agrega los demás usuarios aquí...
-    ('20181020571', 'john', 'Smith', 'john.smith@example.com', 'johnsmith12345');
-    
--- Agrega los demás usuarios aquí...
+
